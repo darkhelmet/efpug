5fc689207c0a8a1f443208c8fde93037396ea07a
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-05-19 20:24:50 -0600

    Fantastic progress! For great win!
    - Play rounds with random dice rolls
    - ASCII art of the progress
    - Scratch horses with costs

--- may-2/horses.hs
+++ may-2/horses.hs
@@ -1,5 +1,7 @@
 import qualified Data.IntMap as M
+import Data.List
 import Data.Maybe
+import System.Random
 
 -- data Suit = Spades | Hearts | Clubs | Diamonds deriving (Eq, Show, Ord)
 
@@ -14,6 +16,9 @@ advanceHorse h = case position h of
                     Scratched _ -> error "can't advance scratched horsed"
                     Distance d  -> h { position = Distance (d + 1) }
 
+scratchHorse :: Int -> Horse -> Horse
+scratchHorse cost horse = horse{position = Scratched cost}
+
 finished :: Horse -> Bool
 finished h = case position h of
                     Scratched _ -> False
@@ -21,6 +26,24 @@ finished h = case position h of
 
 data RoundState = RoundState { horses :: M.IntMap Horse
                              } deriving (Show)
+
+padString :: Int -> String -> String
+padString width str
+    | length str < width = replicate (width - length str) ' ' ++ str
+    | otherwise = str
+
+showLane :: Int -> Int -> String
+showLane 0 len = replicate len '-'
+showLane pos len = replicate (pos - 1) '-' ++ "@" ++ replicate (len - pos) '-'
+
+showHorse :: (Int, Horse) -> String
+showHorse (number, Horse pos fin) = "Horse " ++ padString 2 (show number) ++ ": " ++ suffix
+                                  where suffix = case pos of
+                                                    Scratched c -> replicate fin 'x' ++ " " ++ show c
+                                                    Distance  d -> showLane d fin
+
+showState :: RoundState -> String
+showState = concat . intersperse "\n" . map showHorse . M.toList . horses
 
 makeHorse :: Int -> Horse
 makeHorse finishSpot = Horse{position = Distance 0, finish = finishSpot}
@@ -31,21 +54,51 @@ makeRound = RoundState { horses = M.fromList pairs }
                   lanes = [2..12]
                   lengths = [3,4,5,6,7,8,7,6,5,4,3]
 
+isScratched :: Horse -> Bool
+isScratched horse = case position horse of
+                        Scratched _ -> True
+                        Distance  _ -> False
+
+rollHitScratch :: Int -> RoundState -> Bool
+rollHitScratch number rs = isScratched $ horses rs M.! number
+
+countScratched :: RoundState -> Int
+countScratched rs = length $ filter isScratched $ M.elems $ horses rs
+
+allScratched :: RoundState -> Bool
+allScratched rs = 4 == (countScratched rs)
+
+scratchValue :: RoundState -> Int
+scratchValue rs = 5 * (1 + countScratched rs)
+
 playTurn :: Int -> RoundState -> RoundState
-playTurn number rs = rs{horses = M.adjust advanceHorse number (horses rs)}
+playTurn roll rs
+    | rollHitScratch roll rs = rs
+    | allScratched rs = rs{horses = M.adjust advanceHorse roll (horses rs)}
+    | otherwise = rs{horses = M.adjust (scratchHorse (scratchValue rs)) roll (horses rs)}
 
 winner :: RoundState -> Maybe (Int, Horse)
 winner = listToMaybe . filter (finished . snd) . M.toList . horses
 
-playRound :: [Int] -> [RoundState]
-playRound = takeTurns makeRound
+playRound' :: [Int] -> [RoundState]
+playRound' = takeTurns makeRound
           where takeTurns rs (roll:rolls) = rs : takeTurns (playTurn roll rs) rolls
                 takeTurns rs [] = [rs]
 
--- playRound should return a list of RoundStates up to and including the winning state
+makeDiceRolls :: Int -> [Int]
+makeDiceRolls seed = map (\r -> (r `mod` 6) + 1) $ randoms (mkStdGen seed) :: [Int]
 
--- (head . snd) $ span (isNothing . winner) $ playRound [2,2,2]
+makePlayerRolls :: [Int] -> [Int]
+makePlayerRolls (first:second:rest) = (first + second):makePlayerRolls rest
 
+playRound :: [Int] -> [RoundState]
+playRound rolls = losers ++ [first]
+                 where states = playRound' rolls
+                       (losers,first:_) = span (isNothing . winner) states
+
+-- prettyRound $ makePlayerRolls $ makeDiceRolls 2
+prettyRound :: [Int] -> IO ()
+prettyRound = putStrLn . concat . intersperse "\n=====\n" . map showState . playRound
 
 {-
 1. Get deck of cards, 2 dice
